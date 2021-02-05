c8af7b1125209d64ba615149e599996838703266
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-05 01:45:21 -0300

    Criando um REST Controller de exemplo

--- src/main/java/br/ufrn/bsi/supplies/MyHomeSuppliesApplication.java
+++ src/main/java/br/ufrn/bsi/supplies/MyHomeSuppliesApplication.java
@@ -2,8 +2,6 @@ package br.ufrn.bsi.supplies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class MyHomeSuppliesApplication {
@@ -11,10 +9,4 @@ public class MyHomeSuppliesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyHomeSuppliesApplication.class, args);
 	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
 }
