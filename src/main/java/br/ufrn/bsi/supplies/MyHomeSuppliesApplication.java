bfcb15b44c4d8f3679b2e5f1251f04a628e31170
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-05 00:36:13 -0300

    Add versão gerada pelo start.spring.io

---
+++ src/main/java/br/ufrn/bsi/supplies/MyHomeSuppliesApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.bsi.supplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeSuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeSuppliesApplication.class, args);
+	}
+
+}
