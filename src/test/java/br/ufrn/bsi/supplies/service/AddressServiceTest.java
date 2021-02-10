df3bd78fc6321a4a77f73831f732cc749b9a0ad3
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-10 20:46:41 -0300

    Add teste para o AddressService

---
+++ src/test/java/br/ufrn/bsi/supplies/service/AddressServiceTest.java
@@ -0,0 +1,27 @@
+package br.ufrn.bsi.supplies.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.ufrn.bsi.supplies.entity.Address;
+import br.ufrn.bsi.supplies.service.address.AddressService;
+
+@SpringBootTest()
+public class AddressServiceTest {
+
+    @Autowired
+    AddressService addressService;
+
+    @Test
+    public void testGetAddress() {
+        Optional<Address> address = addressService.search("59300000");
+        assertEquals("Caicó", address.get().getCity());
+        assertEquals("RN", address.get().getState());
+        address = addressService.search("59300000");
+    }
+}
